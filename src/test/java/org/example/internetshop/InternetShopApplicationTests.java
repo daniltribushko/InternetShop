a9123a630bea1afa85506acc156f6fef897e3ff5
daniltribushko <105868780+daniltribushko@users.noreply.github.com>
2024-05-21 21:32:32 +0500

    Initial commit

---
+++ src/test/java/org/example/internetshop/InternetShopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.internetshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
