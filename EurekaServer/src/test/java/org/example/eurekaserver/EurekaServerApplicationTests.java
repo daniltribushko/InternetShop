789231db0fada87db3498f25b0672032a2b1d1a2
daniltribushko <105868780+daniltribushko@users.noreply.github.com>
2024-05-27 17:33:23 +0500

    27.05.2024 Создал модуль сервера Eureka

--- EurekaServer/src/test/java/org/example/eurekaserver/EurekaServerApplicationTests.java
+++ EurekaServer/src/test/java/org/example/eurekaserver/EurekaServerApplicationTests.java
@@ -1,13 +1,13 @@
-package org.example.internetshop;
+package org.example.eurekaserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class InternetShopApplicationTests {
+class EurekaServerApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
